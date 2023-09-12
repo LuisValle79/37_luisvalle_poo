4623888a091cb23546d7a31c694db1153f8b0bc5
LuisAlberto <141541553+LuisValle79@users.noreply.github.com>
2023-09-12 09:49:13 -0500

    Add files via upload

---
+++ Poo2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo2;
+
+/**
+ *
+ * @author Luis
+ */
+public class Poo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        persona objPersona = new persona();
+            objPersona.metodo1();
+            objPersona.metodo1("luis");
+            objPersona.metodoInt();
+            objPersona.metodoString();
+            objPersona.metodoBoolean();
+           
+            
+    
+        
+    }
+    
+}
---
+++ persona.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo2;
+
+/**
+ *
+ * @author Luis
+ */
+public class persona {
+    int a;
+    int b;
+    String c;
+    
+    public void metodo1(){
+        System.out.println("buenos dias");
+    }
+    public void metodo1(String c){
+        System.out.println("buenas tardes " +c);
+    }
+    public int metodoInt(){
+    return 20;
+    }
+    public String metodoString(){
+    return "buenas noches colega";
+    }
+    public boolean metodoBoolean(){
+    return true;
+    }
+    
+    
+    
+            
+}
