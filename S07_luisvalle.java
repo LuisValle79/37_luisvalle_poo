02ef4634e732288527ac70aaead939faf5261ea6
LuisAlberto <141541553+LuisValle79@users.noreply.github.com>
2023-10-03 10:07:57 -0500

    ok

---
+++ S07_luisvalle.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package s07v2_37;
+
+/**
+ *
+ * @author Lab20
+ */
+public class S07_luisvalle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        persona1 p1 = new persona1();
+            p1.jugar("luis");
+            p1.gatear("jeyko", 8);
+            p1.pasear("lucas", 25, 78596412);
+            System.out.println(p1.getEdad());
+        
+    }
+       
+            
+}
--- persona1.java
+++ persona1.java
@@ -1,14 +1,32 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package s07_37luisvalle;
+package s07v2_37;
 
 /**
  *
  * @author Lab20
  */
 public class persona1 {
-    
+
+    public void jugar(String nombre) {
+        System.out.println("hola, me llamo " + nombre + "y me gusta jugar basquet");
+    }
+
+    public void pasear(String nombre, int edad, int dni) {
+        System.out.println("hola, me llamo " + nombre + " y tengo " + edad + " años y mi dni es " + dni);
+    }
+
+    public void gatear(String nombre, int edad) {
+        System.out.println("hola, me llamo " + nombre + " y tengo " + edad + " años");
+    }
+
+    public void jugar(int edad) {
+        System.out.println("hola tengo " + edad + " años");
+    }
+
+    public int getEdad() {
+        return 20;
+    }
 }
